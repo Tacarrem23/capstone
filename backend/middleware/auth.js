e9894fb6d8319129ff2c10b0c5858da59b018a2a
Tacarrem23 <carremitchell729@gmail.com>
2025-04-25 17:33:10 -0400

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+const JWT_SECRET = "your_jwt_secret"; // Replace with an environment variable in production
+
+const authMiddleware = (req, res, next) => {
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "No token provided." });
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token." });
+  }
+};
+
+export default authMiddleware;
